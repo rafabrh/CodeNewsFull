bc105467f506165a139698fb9e4a5b4334759936
rafabrh <rafael.alvarenga1@hotmail.com>
2025-06-27 20:10:50 -0300

    deploy backend without frontend

--- frontend/postcss.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('postcss').ProcessOptions} */
-module.exports = {
-    plugins: {
-        '@tailwindcss/postcss': {},   // usar o plugin separado
-        autoprefixer: {},
-    },
-}
